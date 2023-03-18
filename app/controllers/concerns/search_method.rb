2238af30d12271742e0fef1ce0058ef27d0b517c
Viktor Kukhar <kukharviktor@icloud.com>
2023-03-19 01:40:15 +0200

    functionality without model

--- app/controllers/concerns/search_method.rb
+++ app/controllers/concerns/search_method.rb
@@ -1,29 +1,40 @@
 module SearchMethod
   def search_method(search_term)
-
     # Support for negative searches, e.g. john -array
-    excluded_terms = search_term.scan(/-(\w+)/).flatten
-    excluded_terms.each do |term|
-      @items = @items.where.not("name ILIKE ? OR category ILIKE ? OR designed_by ILIKE ?", "%#{term}%", "%#{term}%", "%#{term}%")
-    end
+      excluded_terms = search_term.scan(/-(\w+)/).flatten
+      @json_data.reject! do |item|
+        excluded_terms.any? do |term|
+          item['Name'].downcase.include?(term) || item['Type'].downcase.include?(term) || item['Designed by'].downcase.include?(term)
+        end
+      end
 
-    # Remove excluded terms from original query
-    search_term = search_term.gsub(/-\w+/, '').strip
+      # Remove excluded terms from original query
+      search_term = search_term.gsub(/-\w+/, '').strip
 
-    # Match in different fields, e.g. Scripting Microsoft
-    # should return all scripting languages designed by "Microsoft"
-    query_terms = search_term.split
-    query_terms.each do |term|
-      @items = @items.where("name ILIKE ? OR category ILIKE ? OR designed_by ILIKE ?", "%#{term}%", "%#{term}%", "%#{term}%")
-    end
+      # Match in different fields, e.g. Scripting Microsoft
+      # should return all scripting languages designed by "Microsoft"
+      query_terms = search_term.split
+      @json_data.select! do |item|
+        query_terms.all? do |term|
+          item['Name'].downcase.include?(term) || item['Type'].downcase.include?(term) || item['Designed by'].downcase.include?(term)
+        end
+      end
 
-     # Search match precision
-     @items = @items.order(Arel.sql("CASE
-                                      WHEN name ILIKE '#{search_term}%' THEN 1
-                                      WHEN category ILIKE '#{search_term}%' THEN 2
-                                      WHEN designed_by ILIKE '#{search_term}%' THEN 3
-                                      ELSE 4
-                                    END"))
+      # Search match precision
+      @json_data.sort_by! do |item|
+        case item
+        when ->(i) { i['Name'].downcase.start_with?(search_term) }
+          1
+        when ->(i) { i['Type'].downcase.start_with?(search_term) }
+          2
+        when ->(i) { i['Designed by'].downcase.start_with?(search_term) }
+          3
+        else
+          4
+        end
+      end
+
+      @json_data
 
     end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,17 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[ show edit update destroy ]
+  before_action :set_json, only: %i[ index search ]
   include SearchMethod
 
-  # GET /items or /items.json
-  def index
-    @items = Item.all
-  end
-
   def search
-    @items = Item.all
-    search_term = params[:search]
+    search_term = params[:search].downcase
     search_method(search_term)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params[:id])
+
+    def set_json
+      json_file = File.read(Rails.root.join('db', 'data.json'))
+      @json_data = JSON.parse(json_file)
     end
 
-    # Only allow a list of trusted parameters through.
-    def item_params
-      params.require(:item).permit(:name, :category, :designed_by, :search)
-    end
 end
