83b49c1b5615d152492a6217556bd6676135a0b9
Viktor Kukhar <kukharviktor@icloud.com>
2023-03-21 01:52:59 +0200

    smaller methods

--- app/controllers/concerns/search_method.rb
+++ app/controllers/concerns/search_method.rb
@@ -1,40 +1,44 @@
 module SearchMethod
+
   def search_method(search_term)
-    # Support for negative searches, e.g. john -array
-      excluded_terms = search_term.scan(/-(\w+)/).flatten
-      @json_data.reject! do |item|
-        excluded_terms.any? do |term|
-          item['Name'].downcase.include?(term) || item['Type'].downcase.include?(term) || item['Designed by'].downcase.include?(term)
-        end
-      end
+     exclude_data_with_terms(search_term)
+     query_data_with_terms(search_term)
+     sort_data_by_match_precision(search_term)
+     @json_data
+   end
 
-      # Remove excluded terms from original query
-      search_term = search_term.gsub(/-\w+/, '').strip
+   private
 
-      # Match in different fields, e.g. Scripting Microsoft
-      # should return all scripting languages designed by "Microsoft"
-      query_terms = search_term.split
-      @json_data.select! do |item|
-        query_terms.all? do |term|
-          item['Name'].downcase.include?(term) || item['Type'].downcase.include?(term) || item['Designed by'].downcase.include?(term)
-        end
-      end
+   def exclude_data_with_terms(search_term)
+     excluded_terms = search_term.scan(/-(\w+)/).flatten
+     excluded_terms.each do |term|
+       @json_data.reject! do |data|
+         data['Name'].downcase.include?(term) || data['Type'].downcase.include?(term) || data['Designed by'].downcase.include?(term)
+       end
+     end
+   end
 
-      # Search match precision
-      @json_data.sort_by! do |item|
-        case item
-        when ->(i) { i['Name'].downcase.start_with?(search_term) }
-          1
-        when ->(i) { i['Type'].downcase.start_with?(search_term) }
-          2
-        when ->(i) { i['Designed by'].downcase.start_with?(search_term) }
-          3
-        else
-          4
-        end
-      end
+   def query_data_with_terms(search_term)
+     query_terms = search_term.gsub(/-\w+/, '').strip.split
+     query_terms.each do |term|
+       @json_data.select! do |data|
+         data['Name'].downcase.include?(term) || data['Type'].downcase.include?(term) || data['Designed by'].downcase.include?(term)
+       end
+     end
+   end
 
-      @json_data
-
-    end
+   def sort_data_by_match_precision(search_term)
+     @json_data.sort_by! do |data|
+       case data
+       when ->(i) { i['Name'].downcase.start_with?(search_term) }
+         1
+       when ->(i) { i['Type'].downcase.start_with?(search_term) }
+         2
+       when ->(i) { i['Designed by'].downcase.start_with?(search_term) }
+         3
+       else
+         4
+       end
+     end
+   end
 end
